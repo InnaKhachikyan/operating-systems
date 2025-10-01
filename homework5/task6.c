12ea134fb5ccbdd446e7cb96bd23cb043ab9f781
InnaKhachikyan <khachikyaninna@gmail.com>
2025-10-01 03:18:53 +0000

    I am trying to implement realloc without malloc_usable_size, getting the size from the headers, but not successful yet

--- homework5/task6.c
+++ homework5/task6.c
@@ -29,6 +29,38 @@ void* my_realloc(void* ptr, size_t new_size) {
 	return new_ptr;
 }
 
+void* my_realloc2(void* ptr, size_t new_size) {
+
+        if(ptr == NULL) {
+                return malloc(new_size);
+        }
+
+        if(new_size == 0) {
+                free(ptr);
+                ptr = NULL;
+                return NULL;
+        }
+
+        //size_t old_size = (size_t)*((uint8_t*)(ptr-8));
+        size_t old_size;
+        memcpy(&old_size, (uint8_t*)(ptr) - 8, 8);
+        old_size = old_size & (~0x07);
+
+        printf("old size by malloc_usable %ld\n", malloc_usable_size(ptr));
+        printf("Size is %ld\n", old_size);
+        void *new_ptr = malloc(new_size);
+        if(!new_ptr) {
+                return NULL;
+        }
+        size_t copy = old_size < new_size ? old_size : new_size;
+
+        memcpy(new_ptr, ptr, copy);
+        free(ptr);
+        ptr = NULL;
+        return new_ptr;
+}
+
+
 int main(void) {
 
 	int *ptr = (int*)malloc(sizeof(int)*16);
